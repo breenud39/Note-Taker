eaa36319775c2aedb3824a7ece7f6b972ab5b5ad
Vaggelis <evangelodamianos@gmail.com>
2022-03-15 20:54:13 -0400

    req works

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { index } = require('./db/db.json')
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -41,6 +42,6 @@ app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../../public/index.html'));
 // });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 })
